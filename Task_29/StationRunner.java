6a1199dd69a419797d1d12304fd2dba1eb26fb97
Shashank K <sharathshashank1986@gmail.com>
2026-03-31 22:56:03 +0530

    Simple(Single) Inheritence

---
+++ Task_29/StationRunner.java
@@ -0,0 +1,7 @@
+class StationRunner{
+	public static void main(String[] values){
+		MetroStation infoOfStation = new MetroStation();
+		infoOfStation.info();
+		infoOfStation.metroStationInfo();
+	}
+}
